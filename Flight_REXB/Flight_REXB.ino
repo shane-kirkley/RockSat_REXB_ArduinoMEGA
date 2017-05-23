5fab4a06a09c7bcea6a16b373a9b59c55e2a5025
abertman <alex.bertman@colorado.edu>
2017-05-23 14:02:36 -0600

    Accel code added. AccelCond and ReadAxis added to bottom, as well as calibration raw values added to top. analogReference set to EXTERNAL

--- Flight_REXB/Flight_REXB.ino
+++ Flight_REXB/Flight_REXB.ino
@@ -1,3 +1,10 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
 /*******************************************************************
   RockSat-X REX-B Flight Software
   (Current) Avionics Team:
@@ -26,6 +33,15 @@
 const int ACCL_X = A0;
 const int ACCL_Y = A1;
 const int ACCL_Z = A2;
+
+int xRawMin = 510;
+int xRawMax = 515;
+int yRawMin = 512;
+int yRawMax = 517;
+int zRawMin = 509;
+int zRawMax = 514;
+
+const int sampleSize = 10;
 
 // TEMP SENSORS
 const int TEMP_C1 = A4;
@@ -113,6 +129,8 @@ void goProTriggerISR();
 void TimerEvent2ISR();
 void downlinkImage();
 void getSensorData(uint16_t * buff); // gets all sensor data and stores in buff.
+int ReadAxis(int axisPin); //Reads 10 samples from 
+float AccelCond(int Raw, int RawMin, int RawMax);
  
 /*
  * Power on setup
@@ -131,6 +149,8 @@ void setup() {
   pinMode(ACCL_X, INPUT);
   pinMode(ACCL_Y, INPUT);
   pinMode(ACCL_Z, INPUT);
+
+  analogReference(EXTERNAL);
 
   // temp and pressure sensor setup
   pinMode(TEMP_E, INPUT);
@@ -297,6 +317,13 @@ void getSensorData(uint16_t * buff) {
 
 void saveData() {
   dataFile = SD.open(filename, FILE_WRITE);
+  int xRaw = ReadAxis(ACCL_X);
+  float xAcc = AccelCond(xRaw,xRawMin,xRawMax);
+  int yRaw = ReadAxis(ACCL_Y);
+  float yAcc = AccelCond(yRaw,yRawMin,yRawMax);
+  int zRaw = ReadAxis(ACCL_Z);
+  float zAcc = AccelCond(zRaw,zRawMin,zRawMax);
+  
   //Serial.println(SDdataBuffer);
   if(dataFile) {
     dataFile.println(SDdataBuffer);
@@ -416,6 +443,23 @@ void TimerEvent2ISR() {
   
 }
 
+float AccelCond(int Raw, int RawMin, int RawMax){
+  float Acc = map(Raw,RawMin,RawMax,-1000,1000)/1000.0;
+  return Acc;
+}
+
+
+int ReadAxis(int axisPin)
+{
+  long reading = 0;
+  analogRead(axisPin);
+  delay(1);
+  for (int i = 0; i < sampleSize; i++)
+    {
+      reading += analogRead(axisPin);
+    }
+  return reading/sampleSize;
+}
 
 
 
